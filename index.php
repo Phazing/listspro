30d50f90d56f4eeb487bf9bbb06853ad9c044280
Eliphaz BOUYE <bouyesamuel@gmail.com>
2022-10-12 00:48:40 +0000

    html struct

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-<h1><?php echo 'Hello, World' ?></h1>
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Lists PRO</title>
+    </head>
+     <body>
+        <div class="container">
+            <h1>List PRO</h1>
+            <div class="list-groups">
+		<div class="list-group-item list-group-item-action list-group-item-secondary rounded">
+			<span>First Name</span>
+			<span>Lastt Name</span>
+			<span>Email</span>
+			<img src="#" alt="simple profile picture"/>
+			<a href="#">Edit</a>
+			<a href="#">Show</a>
+			<a href="#">Delete</a>
+		</div>    
+
+		<div class="list-group-item list-group-item-action list-group-item-primary rounded">
+			<span>First Name</span>
+			<span>Lastt Name</span>
+			<span>Email</span>
+			<img src="#" alt="simple profile picture"/>
+			<a href="#">Edit</a>
+			<a href="#">Show</a>
+			<a href="#">Delete</a>
+		</div>    
+
+		<div class="list-group-item list-group-item-action list-group-item-secondary rounded">
+			<span>First Name</span>
+			<span>Lastt Name</span>
+			<span>Email</span>
+			<img src="#" alt="simple profile picture"/>
+			<a href="#">Edit</a>
+			<a href="#">Show</a>
+			<a href="#">Delete</a>
+		</div>    
+            </div>
+        </div>
+     </body>
+</html>
