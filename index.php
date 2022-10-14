14d73a29212026960fdd5e3aff7ecda8320697ff
Eliphaz BOUYE <bouyesamuel@gmail.com>
2022-10-14 14:58:46 +0000

    display data record

--- index.php
+++ index.php
@@ -7,10 +7,7 @@ try {
 	$connection = new PDO("mysql:host=$servername;dbname=listspro", $username, $password);
 	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	$contactLists = 'SELECT first_name, last_name, address, city FROM contacts ORDER BY id ';
-	foreach($connection->query($contactLists) as $row) {
-		var_dump($row['first_name']);	
-	}
+	$contactLists = 'SELECT firstname, lastname, location, email FROM contacts ORDER BY id ';
 
 } catch(PDOException $e) {
 	echo 'Connection failed ' . $e->getMessage();
@@ -26,35 +23,26 @@ try {
         <div class="container">
             <h1>List PRO</h1>
             <div class="list-groups">
+
+
+	<?php
+	foreach($connection->query($contactLists) as $row) {
+	?>
 		<div class="list-group-item list-group-item-action list-group-item-secondary rounded">
-			<span>First Name</span>
-			<span>Lastt Name</span>
-			<span>Email</span>
-			<img src="#" alt="simple profile picture"/>
+
+			<span><?php echo $row['firstname'] ?></span>
+			<span><?php echo $row['lastname'] ?></span>
+			<span><?php echo $row['location'] ?></span>
+			<span><?php echo $row['email'] ?></span>
 			<a href="#">Edit</a>
 			<a href="#">Show</a>
 			<a href="#">Delete</a>
 		</div>    
 
-		<div class="list-group-item list-group-item-action list-group-item-primary rounded">
-			<span>First Name</span>
-			<span>Lastt Name</span>
-			<span>Email</span>
-			<img src="#" alt="simple profile picture"/>
-			<a href="#">Edit</a>
-			<a href="#">Show</a>
-			<a href="#">Delete</a>
-		</div>    
+	<?php
+	}
+	?>
 
-		<div class="list-group-item list-group-item-action list-group-item-secondary rounded">
-			<span>First Name</span>
-			<span>Lastt Name</span>
-			<span>Email</span>
-			<img src="#" alt="simple profile picture"/>
-			<a href="#">Edit</a>
-			<a href="#">Show</a>
-			<a href="#">Delete</a>
-		</div>    
             </div>
         </div>
      </body>
